3a2941e9a7986c77d03b69513a81cb06d4af4a49
Onyx McQueen <onyxmcqueen88@gmail.com>
2025-03-08 18:13:22 -0500

    Create operate function to correctly handle calculator operations on numbers

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
-// Three variables to store the calculator operation (firstNumber, operation, secondNumber)
+// Three variables to store each part of the calculator operation (firstNumber, operation, secondNumber)
 let firstNumber = '';
 let operator = '';
 let secondNumber = '';
 
 // Create Math Operation Functions (add, subtract, multiply, divide)
-
 function add(num1, num2){
   return num1 + num2;
 }
@@ -22,4 +21,20 @@ function divide(num1, num2){
     return 'Error: Cannot Divide By 0.'
   }
   return num1 / num2;
+}
+
+// Central Function that handles calculator operation logic.
+function operate(operator, num1, num2){
+  switch(operator){
+    case '+':
+      return add(num1, num2);
+    case '-':
+      return subtract(num1, num2);
+    case 'x':
+      return multiply(num1, num2);
+    case '÷':
+      return divide(num1, num2);
+    default:
+      return 'Error: Invalid Operator';
+  }
 }
