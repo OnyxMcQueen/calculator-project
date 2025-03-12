1401bc461e9615a7c5f6d08d1e448e9f2374cbd6
Onyx McQueen <onyxmcqueen88@gmail.com>
2025-03-12 02:43:51 -0400

    Implemented click event handler for digit & operator buttons, updated display logic

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+const calculator = document.querySelector('.calculator');
+const calculatorDisplay = document.querySelector('#display');
+const message = document.querySelector('#message');
+let previousOperator = null;
+
 // Three variables to store each part of the calculator operation (firstNumber, operation, secondNumber)
 let firstNumber = '';
 let operator = '';
@@ -38,3 +43,49 @@ function operate(operator, num1, num2){
       return 'Error: Invalid Operator';
   }
 }
+
+function updateDisplay(number){
+  calculatorDisplay.textContent = number;
+}
+
+// Attach click event listener to entire calculator, listen for button clicks.
+calculator.addEventListener('click', (e) => {
+  if(e.target.tagName === 'BUTTON'){
+    if(e.target.classList.contains('digit')){
+      message.textContent = '';
+      if(operator === ''){
+        firstNumber += e.target.textContent;
+        updateDisplay(firstNumber);
+      } else {
+        secondNumber += e.target.textContent;
+        updateDisplay(secondNumber);
+      }
+    }
+
+    if(e.target.classList.contains('operator')){
+      if(firstNumber === ''){
+        message.textContent = 'It seems like you haven\'t input a number, please type in a number first.';
+        return;
+      }
+
+      if(secondNumber){
+        return;
+      }
+
+      if(previousOperator){
+        if(previousOperator.textContent === e.target.textContent){
+          operator = '';
+          previousOperator.classList.remove('highlight');
+          previousOperator = null;
+          return;
+        } else {
+          previousOperator.classList.remove('highlight');
+        }
+      }
+
+      operator = e.target.textContent;
+      e.target.classList.add('highlight');
+      previousOperator = e.target;
+    }
+  }
+})
