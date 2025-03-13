f67570beb87c9cb6d88e0b5be00201b8540dec03
Onyx McQueen <onyxmcqueen88@gmail.com>
2025-03-13 14:24:06 -0400

    Create functionality for equal button click; Create shouldBeEnabled & convertToNumber function; Add event listener to document to disable equal button on page load

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const calculator = document.querySelector('.calculator');
 const calculatorDisplay = document.querySelector('#display');
 const message = document.querySelector('#message');
 let previousOperator = null;
+const equalButton = document.querySelector('#btn-equal');
 
 // Three variables to store each part of the calculator operation (firstNumber, operation, secondNumber)
 let firstNumber = '';
@@ -23,7 +24,7 @@ function multiply(num1, num2){
 
 function divide(num1, num2){
   if(num2 === 0){
-    return 'Error: Cannot Divide By 0.'
+    return 'I am broken XD';
   }
   return num1 / num2;
 }
@@ -48,11 +49,30 @@ function updateDisplay(number){
   calculatorDisplay.textContent = number;
 }
 
+function convertToNumber(stringNumber){
+  return Number(stringNumber);
+}
+
+function shouldBeEnabled(){
+  if(firstNumber && secondNumber && operator){
+    equalButton.disabled = false
+  } else {
+    equalButton.disabled = true;
+  }
+}
+
+// On page load, disable the equal button
+document.addEventListener('DOMContentLoaded', (e) => {
+  equalButton.disabled = true;
+})
+
 // Attach click event listener to entire calculator, listen for button clicks.
 calculator.addEventListener('click', (e) => {
   if(e.target.tagName === 'BUTTON'){
+
     if(e.target.classList.contains('digit')){
       message.textContent = '';
+
       if(operator === ''){
         firstNumber += e.target.textContent;
         updateDisplay(firstNumber);
@@ -60,6 +80,7 @@ calculator.addEventListener('click', (e) => {
         secondNumber += e.target.textContent;
         updateDisplay(secondNumber);
       }
+      shouldBeEnabled();
     }
 
     if(e.target.classList.contains('operator')){
@@ -77,6 +98,7 @@ calculator.addEventListener('click', (e) => {
           operator = '';
           previousOperator.classList.remove('highlight');
           previousOperator = null;
+          shouldBeEnabled();
           return;
         } else {
           previousOperator.classList.remove('highlight');
@@ -86,6 +108,7 @@ calculator.addEventListener('click', (e) => {
       operator = e.target.textContent;
       e.target.classList.add('highlight');
       previousOperator = e.target;
+      shouldBeEnabled();
     }
 
     if(e.target.classList.contains('clear')){
@@ -99,6 +122,29 @@ calculator.addEventListener('click', (e) => {
       }
 
       calculatorDisplay.textContent = '0';
+      shouldBeEnabled();
+    }
+
+    if(e.target.classList.contains('equal')){
+      let operationResult = operate(operator, convertToNumber(firstNumber), convertToNumber(secondNumber));
+      
+      if(typeof operationResult !== 'string'){
+        operationResult = Math.round(operationResult * 100) / 100;
+        calculatorDisplay.textContent = operationResult;
+      } else {
+        calculatorDisplay.textContent = operationResult;
+      }
+
+      firstNumber = '';
+      operator = '';
+      secondNumber = '';
+
+      if(previousOperator){
+        previousOperator.classList.remove('highlight');
+        previousOperator = null;
+      }
+
+      shouldBeEnabled();
     }
   }
 })
